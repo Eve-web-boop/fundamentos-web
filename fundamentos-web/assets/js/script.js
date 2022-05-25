308cd659e2721f4e9e497a683a53ca8229663599
Eveline Síntia <72822610+Eve-web-boop@users.noreply.github.com>
2022-05-25 16:13:48 -0300

    Add files via upload

---
+++ fundamentos-web/assets/js/script.js
@@ -0,0 +1,84 @@
+/*
+Case = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagName()
+por Id: getElementyById()
+por Nome: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let mapa = document.querySelector('#mapa')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+
+    let txtNome = document.querySelector('#txtNome')
+
+    if (nome.value.length < 3) {
+       txtNome.innerHTML = 'Nome inválido'
+       txtNome.style.color = 'red'
+    }
+    else {
+        txt.innerHTML = 'Nome válido'
+        txt.style.color = 'green'
+        let nomeOk = false
+    }
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.')) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    }
+    else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+        let emailOk = false
+    }
+}
+
+function validaAssunto() {
+    
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Seu texto está muito grande, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'block'
+    }
+    else {
+        txtAssunto.style.display = 'none' 
+        let assuntoOk = false
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert ('Formulário enviado com sucesso!')
+    }
+    else {
+        alert ('Preencha o formulário corretamente antes de enviar...')
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
+
+
+}
